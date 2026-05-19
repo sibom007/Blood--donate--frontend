18277c32b297630e8d11b7c6b4d810be768523f5
sibom007 <vaispark400@gmail.com>
2026-05-19 19:34:24 +0600

    Update environment variables, add SignInView and SignUpView components, and modify tsconfig settings

---
+++ src/feature/auth/components/sign-in-view.tsx
@@ -0,0 +1,7 @@
+export const SignInView = () => {
+  return (
+    <div>
+      <h1>SignInView</h1>
+    </div>
+  );
+};
---
+++ src/feature/auth/components/sign-up-view.tsx
@@ -0,0 +1,7 @@
+export const SignUpView = () => {
+  return (
+    <div>
+      <h1>SignUpView</h1>
+    </div>
+  );
+};
